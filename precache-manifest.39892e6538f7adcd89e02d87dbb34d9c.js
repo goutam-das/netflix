5e84d9298288256c2070c2005e48fb95bd5f450c
Goutam Das <goutamdas676@gmail.com>
2020-09-26 14:49:28 +0530

    Updates

---
+++ precache-manifest.39892e6538f7adcd89e02d87dbb34d9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c6b2e0ca36bec039282e94c89f9b53",
+    "url": "/netflix/index.html"
+  },
+  {
+    "revision": "15f7e8bcae3f1bed724c",
+    "url": "/netflix/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5eb65a488438267cc802",
+    "url": "/netflix/static/js/2.e662507e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/netflix/static/js/2.e662507e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15f7e8bcae3f1bed724c",
+    "url": "/netflix/static/js/main.6f8ce7d0.chunk.js"
+  },
+  {
+    "revision": "2dddb01aa0a4e9e8f0b8",
+    "url": "/netflix/static/js/runtime-main.5625af0b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/netflix/static/media/logo.5d5d9eef.svg"
+  }
+]);
